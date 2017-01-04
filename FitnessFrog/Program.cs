aed94cac9f60d48d6b01bc2870ed471e8f24f804
chris-cornish <csquare2390@gmail.com>
2017-01-04 13:51:58 -0500

    Added prompt for minutes exercised

--- FitnessFrog/Program.cs
+++ FitnessFrog/Program.cs
@@ -8,12 +8,22 @@ namespace FitnessFrog
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            // Prompt user for minutes exercised 
-            // Add minutes exercised to total 
-            // Display total minutes exercised to the screen 
-            // Repeat until user quits  
+            // Prompt user for minutes exercised
+
+            Console.Write("Enter how many minutes you exercised: ");
+            
+            var entry = Console.ReadLine();
+
+            // Add minutes exercised to total
+
+            // Display total minutes exercised to the screen
+
+            Console.WriteLine("You've entered " + entry + " minutes");
+
+            // Repeat until user quits
+
         }
     }
 }
