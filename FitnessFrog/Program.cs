870627ce0da6fb05373ff705dab9639d8469449a
chris-cornish <csquare2390@gmail.com>
2017-01-04 13:40:08 -0500

    Added program planning  notes

--- FitnessFrog/Program.cs
+++ FitnessFrog/Program.cs
@@ -10,6 +10,10 @@ namespace FitnessFrog
     {
         static void Main(string[] args)
         {
+            // Prompt user for minutes exercised 
+            // Add minutes exercised to total 
+            // Display total minutes exercised to the screen 
+            // Repeat until user quits  
         }
     }
 }
